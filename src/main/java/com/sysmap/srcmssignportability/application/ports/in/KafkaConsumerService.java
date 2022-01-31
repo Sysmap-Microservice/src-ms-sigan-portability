d6f2f1a8333a90ac089963f4ef5da97db0ba0d41
rafaelcaputodev <rafaelcaputodev@gmail.com>
2022-01-31 13:33:01 -0300

    implements kafka consumer

--- src/main/java/com/sysmap/srcmssignportability/application/ports/in/KafkaConsumerService.java
+++
@@ -1,8 +0,0 @@
-package com.sysmap.srcmssignportability.application.ports.in;
-
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-
-public interface KafkaConsumerService {
-
-    void consume(ConsumerRecord<String, String> payload);
-}
