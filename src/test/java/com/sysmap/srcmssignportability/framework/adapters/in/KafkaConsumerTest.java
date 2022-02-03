85aeeeac03af895467e828504977c1d3487d210f
Gustavo Riposati <guriposa@gmail.com>
2022-02-03 12:51:54 -0300

    novos testes usando o mockProducerKafka

--- src/test/java/com/sysmap/srcmssignportability/framework/adapters/in/KafkaConsumerTest.java
+++ src/test/java/com/sysmap/srcmssignportability/framework/adapters/in/KafkaConsumerTest.java
@@ -1,8 +1,12 @@
 package com.sysmap.srcmssignportability.framework.adapters.in;
 
-import com.sysmap.srcmssignportability.framework.adapters.in.dto.PortabilityInputKafka;
+import com.sysmap.srcmssignportability.domain.entities.Portability;
+import com.sysmap.srcmssignportability.framework.adapters.in.dto.PortabilityInputKafkaNewForKafkaTests;
+import com.sysmap.srcmssignportability.framework.adapters.in.services.KafkaConsumerServiceImplNewForKafkaTests;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.MockConsumer;
 import org.apache.kafka.clients.consumer.OffsetResetStrategy;
+import org.apache.kafka.common.KafkaException;
 import org.apache.kafka.common.TopicPartition;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,15 +18,12 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.listener.ConsumerSeekAware;
 import org.springframework.test.context.junit4.SpringRunner;
 import com.sysmap.srcmssignportability.SrcMsSignPortabilityApplication;
+
 import java.util.*;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.mockito.Mockito.timeout;
-import static org.mockito.Mockito.verify;
 
 @RunWith(SpringRunner.class)
 
@@ -33,14 +34,75 @@ import static org.mockito.Mockito.verify;
 
 class KafkaConsumerTest {
 
-	MockConsumer<String, String> consumer;
+	private static final String TOPIC = "i28hrd7l-portability";
+	private static final int PARTITION = 0;
+	private MockConsumer<String,String> consumer;
+	private List<PortabilityInputKafkaNewForKafkaTests> updates;
+	private KafkaConsumerServiceImplNewForKafkaTests kafkaConsumerService;
+	private Throwable pollException;
 
 	@Autowired
 	public Config config;
 
 	@BeforeEach
-	public void setUp() {
-		consumer = new MockConsumer<String, String>(OffsetResetStrategy.EARLIEST);
+	void setUp() {
+		consumer = new MockConsumer<>(OffsetResetStrategy.EARLIEST);
+		updates = new ArrayList<>();
+		kafkaConsumerService = new KafkaConsumerServiceImplNewForKafkaTests(consumer,
+				ex -> this.pollException = ex, updates::add);
+	}
+
+	@Test
+	void whenStartingByAssigningTopicPartition_thenExpectUpdatesAreConsumedCorrectly() {
+		consumer.schedulePollTask(() -> consumer.addRecord(record(TOPIC, PARTITION,"931313434","441558478995",null)));
+		consumer.schedulePollTask(() -> kafkaConsumerService.stop());
+
+		HashMap<TopicPartition, Long> startOffsets = new HashMap<>();
+		TopicPartition tp = new TopicPartition(TOPIC, PARTITION);
+		startOffsets.put(tp, 0L);
+		consumer.updateBeginningOffsets(startOffsets);
+
+		kafkaConsumerService.startByAssigning(TOPIC, PARTITION);
+
+		assertThat(consumer.closed()).isTrue();
+		System.out.println(this.updates);
+	}
+
+	@Test
+	void whenStartingBySubscribingToTopic_thenExpectUpdatesAreConsumedCorrectly() {
+		consumer.schedulePollTask(() -> {
+			consumer.rebalance(Collections.singletonList(new TopicPartition(TOPIC, 0)));
+			consumer.addRecord(record(TOPIC, PARTITION,"931313434","441558478995", null));
+		});
+		consumer.schedulePollTask(() -> kafkaConsumerService.stop());
+
+		HashMap<TopicPartition, Long> startOffsets = new HashMap<>();
+		TopicPartition tp = new TopicPartition(TOPIC, PARTITION);
+		startOffsets.put(tp, 0L);
+		consumer.updateBeginningOffsets(startOffsets);
+
+		kafkaConsumerService.startBySubscribing(TOPIC);
+
+		assertThat(updates).asList().isNotNull();
+		assertThat(consumer.closed()).isTrue();
+		System.out.println(this.updates);
+	}
+
+	@Test
+	void whenStartingBySubscribingToTopicAndExceptionOccurs_thenExpectExceptionIsHandledCorrectly() {
+		consumer.schedulePollTask(() -> consumer.setPollException(new KafkaException("poll exception")));
+		consumer.schedulePollTask(() -> kafkaConsumerService.stop());
+
+		HashMap<TopicPartition, Long> startOffsets = new HashMap<>();
+		TopicPartition tp = new TopicPartition(TOPIC, 0);
+		startOffsets.put(tp, 0L);
+		consumer.updateBeginningOffsets(startOffsets);
+
+		kafkaConsumerService.startBySubscribing(TOPIC);
+
+		assertThat(pollException).isInstanceOf(KafkaException.class).hasMessage("poll exception");
+		assertThat(consumer.closed()).isTrue();
+		System.out.println(this.updates);
 	}
 
 	@Test
@@ -53,6 +115,10 @@ class KafkaConsumerTest {
 	public void verifyFirstTopicAlreadyInRemoteKafka() throws InterruptedException {
 		assertThat(config.latch.await(20, TimeUnit.SECONDS)).isTrue();
 		assertThat(config.received.get(0)).isEqualTo("{\"number\":\"931313434\",\"documentNumber\":\"441558478995\",\"portability\":{\"portabilityId\":\"b5e1a821-a637-4a3a-b207-01b9f09abc7a\",\"source\":\"CLARO\",\"target\":\"VIVO\"}}");
+	}
+
+	private ConsumerRecord<String, String> record(String topic, int partition, String number, String documentNumber, Portability portability) {
+		return new ConsumerRecord<>(topic, partition, 0, number, documentNumber);
 	}
 
 	@Configuration
