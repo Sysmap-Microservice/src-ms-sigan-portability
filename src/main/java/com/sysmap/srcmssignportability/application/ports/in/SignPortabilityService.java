bdd9ce0852f0a041bd5436896bdd541799d6d0fd
juliana.spinaSY <juliana.spina@sysmap.com.br>
2022-02-08 15:20:50 -0300

    Removendo import não usado

--- src/main/java/com/sysmap/srcmssignportability/application/ports/in/SignPortabilityService.java
+++ src/main/java/com/sysmap/srcmssignportability/application/ports/in/SignPortabilityService.java
@@ -1,6 +1,5 @@
 package com.sysmap.srcmssignportability.application.ports.in;
 
-import com.sysmap.srcmssignportability.domain.entities.Portability;
 import com.sysmap.srcmssignportability.domain.enums.StatusPortability;
 import com.sysmap.srcmssignportability.framework.adapters.in.dto.PortabilityInputKafka;
 import org.springframework.http.HttpStatus;
