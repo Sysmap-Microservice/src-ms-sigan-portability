038687ab1ef9293932689a3ae000c33af882cd79
Andre Meira <andre.santos@sysmap.com.br>
2022-02-18 15:15:35 -0300

    Remove framework from domain layer

--- src/main/java/com/sysmap/srcmssignportability/domain/entities/Portability.java
+++ src/main/java/com/sysmap/srcmssignportability/domain/entities/Portability.java
@@ -4,13 +4,11 @@ import com.sysmap.srcmssignportability.domain.enums.CellPhoneOperator;
 import com.sysmap.srcmssignportability.domain.enums.StatusPortability;
 import lombok.Builder;
 import lombok.Data;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.UUID;
 
 @Data
 @Builder
-@Document
 public class Portability {
     private UUID portabilityId;
     private CellPhoneOperator source;
